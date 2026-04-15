81e1ece2e6b9eff2cda032367b2d7b6fcea2db24
sarah <sarabd5@alu.edu.gva.es>
2026-04-15 16:02:32 +0200

    Create footer.php

---
+++ footer.php
@@ -0,0 +1,7 @@
+    </main>
+    <footer>
+        <p>© <?php echo date('Y'); ?> - Estudiantes Argelinos en Valencia. Todos los derechos reservados.</p>
+        <p>Proyecto integrador de Administración de Redes - FP</p>
+    </footer>
+</body>
+</html>
